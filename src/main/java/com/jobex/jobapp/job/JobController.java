42be2cf050cb599fce53183fe94d44dc06a10af3
Saurav <sauravsynapse@gmail.com>
2024-11-25 23:20:34 +0530

    First Push

--- src/main/java/com/jobex/jobapp/job/JobController.java
+++ src/main/java/com/jobex/jobapp/job/JobController.java
@@ -1,8 +1,19 @@
 package com.jobex.jobapp.job;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@RestController
 public class JobController {
+
+    private List<Job> jobs = new ArrayList<>();
+
+    @GetMapping("/jobs")
     public List<Job> findAll()
     {
-        retrun jobs;
+        return jobs;
     }
 }
