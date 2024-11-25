bf1fb03e8a9f4db68e848ea167dd1521d51be0b0
Saurav <sauravsynapse@gmail.com>
2024-11-25 23:31:30 +0530

    First Push

--- src/main/java/com/jobex/jobapp/job/JobController.java
+++ src/main/java/com/jobex/jobapp/job/JobController.java
@@ -1,6 +1,8 @@
 package com.jobex.jobapp.job;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.ArrayList;
@@ -15,5 +17,12 @@ public class JobController {
     public List<Job> findAll()
     {
         return jobs;
+    }
+
+    @PostMapping("/jobs")
+    public String createJob(@RequestBody Job job)
+    {
+        jobs.add(job);
+        return "Job added Successfully";
     }
 }
